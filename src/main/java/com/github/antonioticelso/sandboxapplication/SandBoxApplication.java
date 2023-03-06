0ddeb2186c5bfebab717143552918db2cd56a9c1
Antonio <antonio.ti.celso@gmail.com>
2023-03-06 12:02:42 -0300

    First commit - start project

---
+++ src/main/java/com/github/antonioticelso/sandboxapplication/SandBoxApplication.java
@@ -0,0 +1,13 @@
+package com.github.antonioticelso.sandboxapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SandBoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SandBoxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/antonioticelso/sandboxapplication/SandBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.antonioticelso.sandboxapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SandBoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
